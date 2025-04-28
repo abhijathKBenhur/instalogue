7ec0ddab70c63f7c5a7aa23dd287abe5b00001dd
abhijathbenhur <abhijathkbenhur@gmail.com>
2025-04-29 00:05:47 +0530

    add search icon

--- client/src/client/screens/Catalogue/Catalogue.js
+++ client/src/client/screens/Catalogue/Catalogue.js
@@ -202,6 +202,14 @@ function Catalogue(props) {
               onChange={(e) => {
                 setSearchString(e.target.value);
               }}
+              style={{ 
+                fontSize: '16px',
+                paddingLeft: '30px', // Make room for the icon
+                backgroundImage: 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'16\' height=\'16\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'%23999\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\'%3E%3Ccircle cx=\'11\' cy=\'11\' r=\'8\'%3E%3C/circle%3E%3Cline x1=\'21\' y1=\'21\' x2=\'16.65\' y2=\'16.65\'%3E%3C/line%3E%3C/svg%3E")',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '8px center',
+                backgroundSize: '16px'
+              }}
             />
             {searchString && (
               <button
